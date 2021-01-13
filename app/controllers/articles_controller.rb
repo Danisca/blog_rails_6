f58f10eb9afa7b5f5e75bf01245c868924bc64cb
danilol <dcanadablandon@gmail.com>
2021-01-13 11:58:56 -0600

    add bootstrap using webpack

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,49 @@
+class ArticlesController < ApplicationController
+	before_action :find_model, only: [:edit,:show,:update,:delete]
+
+	def index
+		@articles = Article.all
+	end
+
+	def new
+		@article = Article.new
+	end
+
+	def show
+		
+	end
+
+	def create
+		@article = Article.new(articles_params)
+		if @article.save
+			flash[:notice] = "Article was created succesfully"
+			redirect_to @article
+		else
+			render "new"
+		end
+	end
+
+	def update
+		if @article.update(articles_params)
+			flash[:notice] = "Article was updated succesfully"
+			redirect_to @article
+		else
+			render "edit"
+		end
+	end
+
+        def delete
+          @article.destroy()
+          redirect_to articles_path
+        end
+
+	private
+	
+	def find_model
+		@article = Article.find(params[:id]) 
+	end
+
+	def articles_params
+		params.require(:article).permit(:title,:description)
+	end
+end
