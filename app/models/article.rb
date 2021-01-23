c950d78446cd139352df1cb6685c69c7ae3e83a9
danilol <dcanadablandon@gmail.com>
2021-01-23 12:55:15 -0600

    add user association, migration and model validations

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,4 @@
 class Article < ApplicationRecord
+	belongs_to :user
 	validates :title,:description, presence: true
-	
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+	has_many :articles
 	validates :username, presence: true, uniqueness: {case_sensitive: false}, 
 						length: {minimum: 3, maximum: 25} 
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
@@ -6,5 +7,5 @@ class User < ApplicationRecord
 #passed is a valid email or not
 	validates :email, presence: true, uniqueness: true, 
 						length: {minimum: 3, maximum: 125},
-						formant:  {with: VALID_EMAIL_REGEX}
+						format:  {with: VALID_EMAIL_REGEX}
 end
