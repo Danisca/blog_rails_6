38bcffa44d190ce68107a43ebd4c8a1bf4f82157
danilol <dcanadablandon@gmail.com>
2021-01-23 10:42:17 -0600

    add user model and its validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,10 @@
 class User < ApplicationRecord
-	
-	
-	
+	validates :username, presence: true, uniqueness: {case_sensitive: false}, 
+						length: {minimum: 3, maximum: 25} 
+	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+# VALID_EMAIL_REGEX contains the regular expresion to validate if the email 
+#passed is a valid email or not
+	validates :email, presence: true, uniqueness: true, 
+						length: {minimum: 3, maximum: 125},
+						formant:  {with: VALID_EMAIL_REGEX}
 end
