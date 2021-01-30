09229d366f5009722295b1af49c06ff0ead1cc8b
danilol <dcanadablandon@gmail.com>
2021-01-30 12:14:05 -0600

    add user login and logout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,9 +2,22 @@ class SessionsController < ApplicationController
   def new 
     
   end
+
   def create 
-   puts("_______-------") 
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user and user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      flash[:notice] = "Successfully login" 
+      redirect_to user
+    else
+      flash.now[:error] = "There were some errors in your loggin details"
+      render 'new'
+    end
   end
+
   def destroy 
+    session[:user_id] = nil
+    flash[:notice] = "Successfully logged out" 
+    redirect_to root_path
   end
 end
