1cd0a150a5530c5a212e7dd635afe59e91112849
danilol <dcanadablandon@gmail.com>
2021-01-30 11:37:35 -0600

    add sessions routes,controller,views

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,7 @@ Rails.application.routes.draw do
   get 'signup', to: 'users#new'
   # post 'users', to: 'users#create'
   resources 'users', except: [:new]
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  get 'loguot', to: 'sessions#destroy'
 end
